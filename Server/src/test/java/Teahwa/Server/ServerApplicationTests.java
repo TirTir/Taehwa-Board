148cea94b562f07cdc32a4dd0298673e38f8a87a
yujin <uj0791navercom@skuniv.ac.kr>
2025-02-28 13:38:05 +0900

    [CHORE] Initial Config

---
+++ Server/src/test/java/Teahwa/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Teahwa.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
